ac9511eb95e484cafc690aaedc85f2603a2a3feb
Paul Iutzi <pji@mac.com>
2019-12-31 10:17:52 -0600

    Added Sized protocol to Deck. Equality comp to Card and Deck.

--- blackjack/cards.py
+++ blackjack/cards.py
@@ -5,11 +5,21 @@ cards
 The module contains the basic classes used by blackjack for handling 
 cards.
 """
+from collections import OrderedDict
+from itertools import product
+
 from blackjack.model import Boolean, valfactory
 
 # Global values.
 UP = True
 DOWN = False
+RANKS = list(range(1, 14))
+SUITS = OrderedDict([
+    ('clubs', '♣'),
+    ('diamonds', '♦'),
+    ('hearts', '♥'),
+    ('spades', '♠'),
+])
 
 
 # Validator functions.
@@ -21,33 +31,26 @@ def validate_rank(self, value: int) -> int:
         reason = 'value must be able to be an int'
         raise TypeError(self.msg.format(reason))
     else:
-        if normal < 1:
+        if normal < RANKS[0]:
             reason = 'value must be greater than zero'
             raise ValueError(self.msg.format(reason))
-        if normal > 13:
+        if normal > RANKS[-1]:
             reason = 'value must be less than 14'
             raise ValueError(self.msg.format(reason))
         return normal
 
 def validate_suit(self, value):
     """Validate card suits."""
-    suits = [
-        'clubs',
-        'diamonds',
-        'hearts',
-        'spades',
-    ]
-    
     try:
         normal = value.lower()
     except AttributeError:
         pass
     else:
-        if normal in suits:
+        if normal in SUITS:
             return normal
     
     try:
-        return suits[value]
+        return list(SUITS.keys())[value]
     except IndexError:
         pass
     except TypeError:
@@ -96,15 +99,70 @@ class Card:
             12: 'Q',
             13: 'K',
         }
-        suit_translate = {
-            'clubs': '♣',
-            'diamonds': '♦',
-            'hearts': '♥',
-            'spades': '♠',
-        }
         rank = self.rank
         if rank in rank_translate:
             rank = rank_translate[rank]
-        suit = suit_translate[self.suit]
+        suit = SUITS[self.suit]
         return f'{rank}{suit}'
+    
+    def __eq__(self, other):
+        if isinstance(other, Card):
+            return (self.suit == other.suit and self.rank == other.rank)
+        return NotImplemented
+    
+    def __ne__(self, other):
+        result = self.__eq__(other)
+        if isinstance(result, bool):
+            return not result
+        return result
 
+
+class Deck:
+    """A deck of playing cards for blackjack."""
+    def __init__(self, cards: list = None) -> None:
+        """Initialize and instance of the class.
+        
+        :param cards: (Optional.) A list containing the cards held by 
+            the deck.
+        :return: None
+        :rtype: None
+        """
+        self.cards = cards
+        if not self.cards:
+            self.cards = []
+    
+    def __eq__(self, other):
+        if isinstance(other, Deck):
+            return self.cards == other.cards
+        return NotImplemented
+    
+    # Sized protocol.
+    def __len__(self):
+        """Get the number of cards in the deck."""
+        return len(self.cards)
+    
+    # Iterator protocol.
+    
+    
+    # Iterable protocol.
+    # Container protocol.
+    # Collection protocol.
+    # Reversible protocol.
+    # Sequence protocol.
+    # MutableSequence protocol.
+    
+    @classmethod
+    def build(cls, num_decks: int = 1):
+        """(Class method.) Create a Deck object that is populated 
+        with cards.
+        
+        :param num_decks: (Optional.) The number of standard decks to 
+            use when constructing the deck.
+        :return: An instance of Deck that contains Card objects.
+        :rtype: Deck
+        """
+        d = cls()
+        std_deck = [Card(rank, suit) for rank, suit in product(RANKS, SUITS)]
+        for i in range(num_decks):
+            d.cards.extend(std_deck)
+        return d
--- tests/test_cards.py
+++ tests/test_cards.py
@@ -10,6 +10,7 @@ from unittest.mock import call, Mock
 
 from blackjack import cards
 
+
 class CardTestCase(unittest.TestCase):
     def test_exists(self):
         """A class named Card should exist."""
@@ -77,13 +78,177 @@ class CardTestCase(unittest.TestCase):
         self.assertEqual(expected, actual)
     
     def test___str__(self):
-        """Card should return a string suitable for printing."""
+        """Card objects should return a string suitable for printing."""
         expected = 'Q♥'
         
         c = cards.Card(12, 'hearts')
         actual = str(c)
         
         self.assertEqual(expected, actual)
+    
+    def test_equality_test(self):
+        """Card objects should be compared for equality based on their 
+        rank and suit.
+        """
+        c1 = cards.Card(2, 'hearts')
+        c2 = cards.Card(2, 'hearts')
+        c3 = cards.Card(11, 'clubs')
+        c4 = cards.Card(2, 'spades')
+        c5 = cards.Card(11, 'hearts')
+        
+        self.assertTrue(c1 == c2)
+        self.assertFalse(c1 == c3)
+        self.assertFalse(c1 == c4)
+        self.assertFalse(c1 == c5)
+    
+    def test_equality_test_not_implemented(self):
+        """Card objects should return not implemented when compared to 
+        objects that aren't Card objets.
+        """
+        expected = NotImplemented
+        
+        c1 = cards.Card(2, 'hearts')
+        other = 23
+        actual = c1.__eq__(other)
+        
+        self.assertEqual(expected, actual)
+    
+    def test_nonequality_test(self):
+        """Card objects should compare for non equality based on their 
+        rank and suit.
+        """
+        c1 = cards.Card(2, 'hearts')
+        c2 = cards.Card(2, 'hearts')
+        c3 = cards.Card(11, 'clubs')
+        c4 = cards.Card(2, 'spades')
+        c5 = cards.Card(11, 'hearts')
+        
+        self.assertFalse(c1 != c2)
+        self.assertTrue(c1 != c3)
+        self.assertTrue(c1 != c4)
+        self.assertTrue(c1 != c5)
+
+
+class DeckTestCase(unittest.TestCase):
+    def test_exists(self):
+        """A class named Deck should exist."""
+        names = [item[0] for item in inspect.getmembers(cards)]
+        self.assertTrue('Deck' in names)
+    
+    def test_can_instantiate(self):
+        """An instance of Deck should be able to be instantiated."""
+        expected = cards.Deck
+        actual = cards.Deck()
+        self.assertTrue(isinstance(actual, expected))
+    
+    def test_cards(self):
+        """An instance of Deck should be able to hold cards in its 
+        cards attribute.
+        """
+        expected = [
+            cards.Card(1, 3),
+            cards.Card(2, 3),
+            cards.Card(3, 3),
+        ]
+        
+        d = cards.Deck(expected)
+        actual = d.cards
+        
+        self.assertEqual(expected, actual)
+    
+    def test_build(self):
+        """Deck.build() should return an instance of deck that 
+        contains the cards needed for a blackjack game.
+        """
+        expected_size = 52
+        expected_class = cards.Card
+        
+        d = cards.Deck.build()
+        actual_size = len(d.cards)
+        
+        self.assertEqual(expected_size, actual_size)
+        for card in d.cards:
+            self.assertTrue(isinstance(card, expected_class))
+    
+    def test_build_casino_deck(self):
+        """If given the number of standard decks to use, Deck.build() 
+        should return a Deck object that contains cards from that many 
+        standard decks.
+        """
+        num_decks = 6
+        expected_size = 52 * num_decks
+        expected_class = cards.Card
+        
+        d = cards.Deck.build(6)
+        actual_size = len(d.cards)
+        
+        self.assertEqual(expected_size, actual_size)
+        for card in d.cards:
+            self.assertTrue(isinstance(card, expected_class))
+    
+    def test_sized_protocol(self):
+        """Deck should implement the Sized protocol by returning the 
+        number of cards in the deck for Deck.__len__."""
+        expected = 52
+        
+        d = cards.Deck.build()
+        actual = len(d)
+        
+        self.assertEqual(expected, actual)
+    
+    def test_equality_test(self):
+        """Decks that contain the same cards in the same order should 
+        be equal.
+        """
+        d1 = cards.Deck([
+            cards.Card(1, 3),
+            cards.Card(2, 3),
+            cards.Card(3, 3),
+        ])
+        d2 = cards.Deck([
+            cards.Card(1, 3),
+            cards.Card(2, 3),
+            cards.Card(3, 3),
+        ])
+        d3 = cards.Deck([
+            cards.Card(1, 2),
+            cards.Card(2, 3),
+            cards.Card(3, 3),
+        ])
+        d4 = cards.Deck([
+            cards.Card(1, 3),
+            cards.Card(3, 3),
+        ])
+        d5 = cards.Deck([
+            cards.Card(3, 3),
+            cards.Card(1, 3),
+            cards.Card(2, 3),
+        ])
+        
+        self.assertTrue(d1 == d2)
+        self.assertFalse(d1 == d3)
+        self.assertFalse(d1 == d4)
+        self.assertFalse(d1 == d5)
+    
+    def test_equality_notimplemented(self):
+        """Attempts to compare a Deck object with a non-Deck object 
+        should return NotImplemented.
+        """
+        expected = NotImplemented
+        
+        d1 = cards.Deck([
+            cards.Card(1, 3),
+            cards.Card(2, 3),
+            cards.Card(3, 3),
+        ])
+        other = 'spam'
+        actual = d1.__eq__(other)
+        
+        self.assertEqual(expected, actual)
+    
+    def test_copy(self):
+        """Deck.copy() should return a copy of the Deck object."""
+        
 
 
 class validate_rankTestCase(unittest.TestCase):
