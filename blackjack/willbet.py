940d7cb4186c611e707fa5387e4a6b74f19df5db
Paul J. Iutzi <pji@mac.com>
2022-08-06 11:50:10 -0500

    Added more willbets and restructured willbet tests.

--- blackjack/willbet.py
+++ blackjack/willbet.py
@@ -15,6 +15,19 @@ determine whether the player will hit or stand. They must:
 from blackjack.game import Engine
 
 
+def will_bet_dealer(self, engine: Engine) -> int:
+    """The dealer cannot bet."""
+    msg = 'Dealer cannot bet.'
+    raise TypeError(msg)
+
+
 def will_bet_max(self, engine: Engine) -> int:
     """The player always bets the maximum amount."""
+    if self.chips < engine.bet_max:
+        return self.chips
     return engine.bet_max
+
+
+def will_bet_min(self, engine: Engine) -> int:
+    """The player always bets the minimum amount."""
+    return engine.bet_min
--- tests/test_willbet.py
+++ tests/test_willbet.py
@@ -7,7 +7,7 @@ This module contains the unit tests for the blackjack.willbet module.
 :copyright: (c) 2020 by Paul J. Iutzi
 :license: MIT, see LICENSE for more details.
 """
-from functools import partial
+from types import MethodType
 import unittest as ut
 
 from blackjack import players, game, willbet
@@ -16,24 +16,63 @@ from blackjack import players, game, willbet
 class WillBetTestCase(ut.TestCase):
     def setUp(self):
         self.engine = game.Engine()
+        self.player = players.Player(chips=1000)
+
+    def returned_value_test(self, fn, exp):
+        # Test data and state.
+        self.player.will_bet = MethodType(fn, self.player)
+
+        # Run test.
+        act = self.player.will_bet(self.engine)
+
+        # Determine test result.
+        self.assertEqual(exp, act)
 
 
 # Test cases.
+class WillBetDealerTestCase(WillBetTestCase):
+    def test_will_raise_error(self):
+        """When called as the will_bet method of a Player object
+        with a game.Engine, will_bet_dealer will raise a TypeError.
+        """
+        # Expected value.
+        exp_ex = TypeError
+        exp_msg = 'Dealer cannot bet.'
+
+        # Test data.
+        fn = willbet.will_bet_dealer
+
+        # Run test and determine result.
+        with self.assertRaisesRegex(exp_ex, exp_msg):
+            self.returned_value_test(fn, None)
+
+
 class WillBetMaxTestCase(WillBetTestCase):
     def test_will_always_return_maximum_bet(self):
         """When called as the will_bet method of a Player object
         with a game.Engine, will_bet_max will return the maximum
         bet allowed by the game.
         """
-        # Expected value.
         exp = self.engine.bet_max
+        fn = willbet.will_bet_max
+        self.returned_value_test(fn, exp)
 
-        # Test data and state.
-        player = players.Player()
-        player.will_bet = partial(willbet.will_bet_max, None)
+    def test_will_not_bet_more_chips_than_has(self):
+        """If the maximum bet is higher than the amount the player
+        has, the player will bet all their remaining chips.
+        """
+        exp = 96
+        self.player.chips = exp
+        fn = willbet.will_bet_max
+        self.returned_value_test(fn, exp)
 
-        # Run test.
-        act = player.will_bet(self.engine)
 
-        # Determine test result.
-        self.assertEqual(exp, act)
+class WillBetMinTestCase(WillBetTestCase):
+    def test_will_always_return_maximum_bet(self):
+        """When called as the will_bet method of a Player object
+        with a game.Engine, will_bet_min will return the minimum
+        bet allowed by the game.
+        """
+        exp = self.engine.bet_min
+        fn = willbet.will_bet_min
+        self.returned_value_test(fn, exp)
